302bffa568f3c5b3c3a31f4107e7467d67fc7b0e
Claudio Garcia Iglesia <claudio@fsdev.wonderbyte>
2021-02-19 11:40:53 -0300

    finish video

--- app.js
+++ app.js
@@ -92,10 +92,8 @@ app.post('/categoria', async (req, res) => {
         }
         //Guardo la nueva acategoria
         query = `INSERT INTO categoria (nombre) VALUE ("${req.body.nombre.toUpperCase()}")`;
-        console.log(query);
         queryRes = await qy(query);
 
-        console.log(queryRes);
         res.send({ 'respuesta': queryRes.insertId });
 
     } catch (e) {
@@ -104,10 +102,92 @@ app.post('/categoria', async (req, res) => {
     }
 });
 
+app.put('/categoria/:id', async (req, res) => {
+    try {
+        if (!req.body.nombre) {
+            throw new Error('No se envio el nombre');
+        }
+        let query = `SELECT * FROM categoria WHERE nombre = "${req.body.nombre}" and id <> "${req.params.id}"`;
+        queryRes = await qy(query);
+
+        if (queryRes.length > 0) {
+            throw new Error(`La categoria ${req.body.nombre} ya existe`);
+        }
+
+        query = `UPDATE categoria SET nombre = "${req.body.nombre}" WHERE id = "${req.params.id}"`;
+        queryRes = await qy(query);
+
+        res.send({ 'respuesta': queryRes.changedRows });
+        console.log(queryRes);
+
+    } catch (e) {
+        console.log(e.message);
+        res.status(413).send({ "Error": e.message });
+    }
+});
+
+app.delete('/categoria/:id', async (req, res) => {
+    try {
+
+        let query = `SELECT * FROM producto WHERE categoria_id = "${req.params.id}"`;
+        queryRes = await qy(query);
+
+        console.log(queryRes);
+        if (queryRes.length > 0) {
+            throw new Error(`No es posible borrar la categoria, existen uno o mas productos relacionados`);
+        }
+
+        query = `DELETE FROM categoria WHERE id = "${req.params.id}"`;
+        queryRes = await qy(query);
+
+        res.send({ 'respuesta': queryRes });
+
+    } catch (e) {
+        console.log(e.message);
+        res.status(413).send({ "Error": e.message });
+    }
+
+
+});
+
 
 /*
 *  PRODUCTOS
 */
+
+app.post('/producto', async (req, res) => {
+    try {
+        if (!req.body.nombre || !req.body.categoria_id) {
+            throw new Error('No se enviaron los datos necesarios');
+        }
+
+        let query = `SELECT * FROM categoria WHERE id = "${req.body.categoria_id}"`;
+        let queryRes = await qy(query);
+
+        if (queryRes.length == 0) {
+            throw new Error("No existe la categoria");
+        }
+
+        query = `SELECT * FROM producto WHERE nombre = "${req.body.nombre}"`;
+        queryRes = await qy(query);
+
+        if (queryRes.length > 0) {
+            throw new Error("El nombre  de producto ya existe");
+        }
+
+        query = `INSERT INTO producto (nombre, descripcion, categoria_id) VALUES ("${req.body.nombre}", ${req.body.descripcion ? '"' + req.body.descripcion + '"' : null}, ${req.body.categoria_id})`;
+        queryRes = await qy(query);
+
+
+        res.send({ 'respuesta': queryRes.insertId });
+
+
+    } catch (e) {
+        console.log(e.message);
+        res.status(413).send({ "Error": e.message });
+    }
+});
+
 
 /*
 * LISTA DE COMPRAS
